ca2d37bb9af08ecbb9d4a0c38be331f80d3de069
uid <u@uid.bz>
2021-06-09 09:29:26 +0200

    Update update script

--- update.sh
+++ update.sh
@@ -6,18 +6,33 @@ cd tiedb
 go get -u .
 cd ../metadata
 go get -u .
-cd ../client
-go get -u .
 cd ../request
 go get -u .
+go get git.sr.ht/~uid/tie/tiedb@latest
+cd ../client
+go get -u .
+go get git.sr.ht/~uid/tie/tiedb@latest
+go get git.sr.ht/~uid/tie/metadata@latest
+go get git.sr.ht/~uid/tie/client@latest
 cd ../cmd/tie
 go get -u .
+go get git.sr.ht/~uid/putlib@latest
+go get git.sr.ht/~uid/tie/client@latest
+go get git.sr.ht/~uid/tie/request@latest
+go get git.sr.ht/~uid/tie/tiedb@latest
 cd ../tie-daemon
 go get -u .
+go get git.sr.ht/~uid/tie/request@latest
+go get git.sr.ht/~uid/tie/tiedb@latest
 cd ../../gui/component
 go get -u .
+go get git.sr.ht/~uid/tie/client@latest
+go get git.sr.ht/~uid/tie/request@latest
 cd ../tie-tag
 go get -u .
+go get git.sr.ht/~uid/tie/component@latest
+go get git.sr.ht/~uid/tie/client@latest
+go get git.sr.ht/~uid/putlib@latest
 git add -A
 git commit -m "Update modules to latest versions"
 git push
