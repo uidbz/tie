6c5367fd6c010d718fb4bcca939687b3fd53a837
uid <u@uid.bz>
2021-09-25 22:31:40 +0200

    getlib: Add initial caching

--- io/getlib/getlib.go
+++ io/getlib/getlib.go
@@ -10,7 +10,15 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
+)
+
+// Used for cache path
+const (
+	lvlDeep  = 3
+	dirWidth = 2
+	max      = lvlDeep * dirWidth
 )
 
 type TieFunc interface {
@@ -21,6 +29,61 @@ func DownloadFile(url string, sourceHash string, destination string) (err error)
 	d := Download{destination: destination}
 
 	return ExecForEach(url, sourceHash, &d, "")
+}
+
+type Cache struct {
+	CacheDir    string
+	SizeLimit   int64  // Max bytes
+	HistoryFile string // Contain hash and file sizes
+}
+
+func InitCache(customLocation string) Cache {
+	c := Cache{}
+	c.SizeLimit = 0
+	historyFile := ".getlib-cache-history"
+
+	if customLocation != "" {
+		c.CacheDir = customLocation
+	} else {
+		switch runtime.GOOS {
+		case "linux":
+			c.CacheDir, _ = os.UserCacheDir()
+
+		case "windows":
+			c.CacheDir, _ = os.UserCacheDir()
+
+		case "android":
+			c.CacheDir = filepath.Join(os.Getenv("FILESDIR"), "cache")
+		}
+	}
+
+	c.HistoryFile = filepath.Join(c.CacheDir, historyFile)
+
+	return c
+}
+
+func (c *Cache) ReadFile(url string, sourceHash string) (file io.Reader, err error) {
+	var dest string = c.CacheDir
+
+	max := lvlDeep * dirWidth
+	for i := 0; i <= max; i = i + dirWidth {
+		dest = filepath.Join(dest, sourceHash[i:i+dirWidth])
+	}
+	dir := dest
+	dest = filepath.Join(dest, sourceHash)
+	if _, exist := os.Stat(dest); os.IsNotExist(exist) {
+		os.MkdirAll(dir, 0755)
+		out, err := os.Create(dest)
+		if err != nil {
+			return nil, err
+		}
+		f, err2 := ReadFile(url, sourceHash)
+		r := io.TeeReader(f, out)
+
+		return r, err2
+	} else {
+		return os.Open(dest)
+	}
 }
 
 type Download struct {
