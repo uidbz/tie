5231bfa7eb98280d375114c2476f57d1c6951576
uid <u@uid.bz>
2021-10-11 09:21:31 +0200

    Revert to Size() function

--- tiedb/collection.go
+++ tiedb/collection.go
@@ -539,7 +539,7 @@ func (ic *InternalCollection) AssociateExt(entry1, relation_collection, relation
 }
 
 func (ic *InternalCollection) SetToString(value string, relationFilter string, s *Tree) (*StringSliceSet, []*Tree) {
-	size := s.count
+	size := s.Size()
 
 	set := StringSliceSet{Item: value,
 		Key:    make([]string, size),
