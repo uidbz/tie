4f55a3b3b4aabb0c9eb0be964ddf4a9b729b2d88
uid <u@uid.bz>
2021-10-26 09:20:12 +0200

    tie-handle: Add job status feature

--- cmd/tie-handle/main.go
+++ cmd/tie-handle/main.go
@@ -2,10 +2,12 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -24,6 +26,11 @@ type ExternalApp struct {
 	settings   string
 	processDir bool
 	debug      bool
+	force      bool
+	firstLog   bool
+	statusDir  string
+	statusFile string
+	logFile    *os.File
 }
 
 func (app *ExternalApp) Run(file io.Reader, relPath string) (err error) {
@@ -63,12 +70,41 @@ func (app *ExternalApp) Process(path string) {
 		fmt.Println(app.name, cmdArgs)
 	}
 	cmd := exec.Command(app.name, cmdArgs...)
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		fmt.Println(string(out))
-		fmt.Println("Error processing data:", err.Error())
-	} else {
-		fmt.Println(string(out))
+	outpPipe, _ := cmd.StdoutPipe()
+	outputStream := bufio.NewScanner(outpPipe)
+	errPipe, _ := cmd.StderrPipe()
+	errStream := bufio.NewScanner(errPipe)
+	cmd.Start()
+
+	for outputStream.Scan() {
+		app.LogOutput(outputStream.Text())
+	}
+	for errStream.Scan() {
+		app.LogOutput(errStream.Text())
+	}
+	cmd.Wait()
+
+	// out, err := cmd.CombinedOutput()
+	// if err != nil {
+	// 	fmt.Println(string(out))
+	// 	fmt.Println("Error processing data:", err.Error())
+	// } else {
+	// 	fmt.Println(string(out))
+	// }
+}
+
+func (app *ExternalApp) LogOutput(msg string) {
+	fmt.Println(msg)
+
+	if app.statusFile != "" {
+		if app.firstLog {
+			app.firstLog = false
+		} else {
+			msg = "\n" + msg
+		}
+		if _, err := app.logFile.WriteString(msg); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
 
@@ -97,7 +133,7 @@ func (app *ExternalApp) VerifyCleanInput() error {
 }
 
 func main() {
-	urlPtr := flag.String("url", "http://localhost:1162", "tie-serve url")
+	urlPtr := flag.String("url", "https://localhost:1162", "tie-serve url")
 	appPtr := flag.String("app", "", "application to execute")
 	argsPtr := flag.String("args", "", "args to app")
 	inputPtr := flag.String("input", "", "input hash")
@@ -105,6 +141,8 @@ func main() {
 	tmpDirPtr := flag.String("tempdir", "/tmp/tie-handle", "temp dir")
 	processDirPtr := flag.Bool("processdir", false, "process dir, instead of individidual files")
 	debugPtr := flag.Bool("debug", false, "Enable verbose output")
+	forcePtr := flag.Bool("force", false, "Ignore previous job status")
+	statusDirPtr := flag.String("statusdir", "", "Write job status to file/check current status")
 
 	flag.Parse()
 
@@ -116,6 +154,9 @@ func main() {
 		tmpDir:     filepath.Join(*tmpDirPtr, *inputPtr+*settingsPtr),
 		processDir: *processDirPtr,
 		debug:      *debugPtr,
+		force:      *forcePtr,
+		firstLog:   true,
+		statusDir:  *statusDirPtr,
 	}
 	app.output = app.tmpDir + "-output"
 
@@ -128,8 +169,36 @@ func main() {
 		app.PrintDebugInfo()
 	}
 
+	if app.statusDir != "" {
+		app.statusDir = filepath.Join(app.statusDir, *inputPtr+*settingsPtr)
+		app.statusFile = filepath.Join(app.statusDir, "tie-handle-status")
+		if _, err := os.Stat(app.statusFile); err == nil {
+			if app.force {
+				os.Remove(app.statusFile)
+			} else {
+				if data, err := os.ReadFile(app.statusFile); err != nil {
+					fmt.Println("Status files exists, but read-error happened:", err.Error())
+					return
+				} else {
+					fmt.Println(string(data))
+					return
+				}
+			}
+		}
+
+		if err := os.MkdirAll(app.statusDir, 0755); err != nil {
+			fmt.Println("Error making status directory: " + err.Error())
+			return
+		}
+		var err error
+		app.logFile, err = os.OpenFile(app.statusFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
 	if err := os.MkdirAll(app.output, 0755); err != nil {
-		fmt.Println("Error making output directory: " + err.Error())
+		app.LogOutput("Error making output directory: " + err.Error())
 		return
 	}
 
@@ -137,7 +206,7 @@ func main() {
 		app.settings = filepath.Join(app.tmpDir, "tie-handle-settings.json")
 		err := getlib.DownloadFile(app.url, app.inputHash, app.settings)
 		if err != nil {
-			fmt.Println("Error downloading settings file:", err.Error())
+			app.LogOutput("Error downloading settings file: " + err.Error())
 			return
 		}
 	}
@@ -145,19 +214,20 @@ func main() {
 	if *processDirPtr {
 		err := getlib.DownloadFile(app.url, app.inputHash, app.tmpDir)
 		if err != nil {
-			fmt.Println("Error downloading input:", err.Error())
+			app.LogOutput("Error downloading input: " + err.Error())
 		}
 		app.Process(app.tmpDir)
 	} else {
 		getlib.ExecForEach(app.url, app.inputHash, &app, "")
 	}
-
 	status := putlib.Upload(app.url, app.output, putlib.PutConfig{PathToWorkdir: true})
 	if status.ErrorMsg != "" {
-		fmt.Println("Error uploading output:", status.ErrorMsg)
+		app.LogOutput("Error uploading output: " + status.ErrorMsg)
 	}
 
-	fmt.Println("tie-handle-result:", status.LastItem.Hash)
+	app.LogOutput("tie-handle-result: " + status.LastItem.Hash)
+
+	app.logFile.Close()
 
 	os.RemoveAll(app.tmpDir)
 	os.RemoveAll(app.output)
